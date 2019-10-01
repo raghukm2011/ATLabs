ee77f49a35ec6d0040ea187b7544c7e0914076fd
haampus <kannanramaswami@gmail.com>
2019-10-01 19:35:20 +0530

    first commit4

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', './public/index.html'));
+ res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT);
